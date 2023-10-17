23795d6113903e556d05b9178e295f46a0157eae
Philip <filipsenklon@gmail.com>
2023-10-18 00:50:08 +0200

    Dodaj pliki projektów.

---
+++ Project1.cpp
@@ -0,0 +1,42 @@
+﻿// Project1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number0, number1, result;
+	char operation;
+
+	cout << "Podaj liczbe a: ";
+	cin >> number0;
+	cout << "Co mam zrobic z liczbami? : ";
+	cin >> operation;
+	cout << "Podaj liczbe b: ";
+	cin >> number1;
+
+	switch (operation)
+	{
+		case '+':
+			result = number0 + number1;
+			cout << "result equal: " << result;
+			break;
+		case '-':
+			result = number0 - number1;
+			cout << "result equal: " << result;
+			break;
+		case '*':
+			result = number0 * number1;
+			cout << "result equal: " << result;
+			break;
+		case '/':
+			result = number0 / number1;
+			cout << "result equal: " << result;
+			break;
+		default:
+			cout << "Nasz kalkulator jest zbyt słaby na takie skomplikowanie obliczenia Przepraszamy!";
+	}
+}
+
